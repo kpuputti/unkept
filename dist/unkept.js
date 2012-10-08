f804cbfb15e93730930d27015866edb9812194b8
Kimmo Puputti <kpuputti@gmail.com>
2012-10-08 16:23:04 +0300

    Add basic AMD outline.

--- dist/unkept.js
+++ dist/unkept.js
@@ -2,3 +2,20 @@
 * https://github.com/kpuputti/unkept
 * Copyright (c) 2012 Kimmo Puputti <kpuputti@gmail.com>; Licensed MIT */
 
+(function () {
+
+    'use strict';
+
+    var unkept;
+
+
+
+    // Expose as AMD module or as a window global.
+    if (typeof define === 'function' && define.amd) {
+        define(function () {
+            return unkept;
+        });
+    } else {
+        window.unkept = unkept;
+    }
+}());
